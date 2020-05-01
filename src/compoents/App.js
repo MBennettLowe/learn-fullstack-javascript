09605469048515cc54c56c1840ce79a223632bc3
MBennettLowe <mbennettlowe@gmail.com>
2020-04-30 20:15:47 -0400

    Lifecycle Components

--- src/compoents/App.js
+++ src/compoents/App.js
@@ -3,16 +3,54 @@ import Header from './header';
 
 
 // unable to have the <div> tag follow after another React.createElement call <h2> will have to wrap both in a top level <div>
-const App = () => { // props variable passed as an argument to the function
+class App extends React.Component { // introducing State on the Component
+    state = { // Can also use a class property on the state
+        pageHeader: 'Naming Contest'
+    }; 
+    componentDidMount() { // lifecycle method that guarantes the DOM has been mounted in the Browser successfully 
+        console.log('did mount');
+        debugger;
+    }
+    componentWillUnmount() { // lifecycle method that says a component is about to be Unmounted
+      console.log('Will Unmount'); // for this example, we will force the component to unmount by adding a timer to App
+      debugger;
+    }
+
+    render() {
     return (
     <div> 
         <Header message="Naming Contest" />
         <div>
-            ...
+            {this.state.pageHeader}
         </div>
     </div>
 
-    );
+    )};
 };
+// Constructor Example
+// class App extends React.Component { // introducing State on the Component
+//     constructor(props) {
+//         super(props);
+//         this.state = { test: 42 };
+//     }
+// 
+// Can also use the state class property 
+//class App extends React.Component {
+//state = { // Can also use a class property on the state
+   // test: 42 
+// };
+
+//     render() {
+//     return (
+//     <div> 
+//         <Header message="Naming Contest" />
+//         <div>
+//             {this.state.test}
+//         </div>
+//     </div>
+
+//     )};
+// };
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -7,3 +7,11 @@ ReactDOM.render(
     <App />, // headerMessage is a variable, in React their called props!
     document.getElementById('root')
     );
+
+    // This can be tested in the Dev tools console. 
+    setTimeout(() => {
+        ReactDOM.render( // we're forcing the App to render another element. It's going to remove the App component from the DOM and render this.
+            <h2>...</h2>,
+            document.getElementById('root')
+        );
+     },4000); // 4 seconds timer
